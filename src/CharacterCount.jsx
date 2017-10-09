3ba27479a6984e77a8485703548c6dbf41aaa11c
Pontus Persson <pontus.alexander@gmail.com>
2017-10-09 08:42:42 +0200

    Update example component.

---
+++ src/CharacterCount.jsx
@@ -0,0 +1,31 @@
+import React, {Component} from 'react';
+
+class 
+CharacterCount 
+extends 
+Component {
+  constructor
+  (props) {
+    super(props);
+
+    this.state = {
+      characterCount: 0,
+    };
+  }
+
+  handleInputChange=(event)=>{
+    this.setState({
+      characterCount: event.target.value.length,
+    });
+  }
+
+  render() {
+    return <div className="CharacterCount">
+      <input 
+      type='text'
+      onChange={this.handleInputChange}
+      >
+      </input>
+      </div>;
+  }
+}
