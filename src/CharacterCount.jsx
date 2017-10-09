ac0b4210d918d9fcbaf6518e47ae8283a4f68663
Pontus Persson <pontus.alexander@gmail.com>
2017-10-09 10:07:40 +0200

    Fix formatting.

--- src/CharacterCount.jsx
+++ src/CharacterCount.jsx
@@ -1,27 +1,23 @@
-import React, {Component} from 'react';
+import React, { Component } from "react";
 
-class
-CharacterCount 
-extends 
-Component {
-constructor
-(props) {
-  super(props);
+class CharacterCount extends Component {
+  constructor(props) {
+    super(props);
 
-  this.state = {
-    characterCount: 0,
+    this.state = {
+      characterCount: 0
+    };
+  }
+
+  handleInputChange = event => {
+    this.setState({ characterCount: event.target.value.length });
   };
-}
 
-handleInputChange=(event)=>{this.setState({characterCount: event.target.value.length});}
-
-render() {
-  return <div className="CharacterCount">
-    <input 
-    type='text'
-    onChange={this.handleInputChange}
-    >
-    </input>
-    </div>;
-}
+  render() {
+    return (
+      <div className="CharacterCount">
+        <input type="text" onChange={this.handleInputChange} />
+      </div>
+    );
+  }
 }
