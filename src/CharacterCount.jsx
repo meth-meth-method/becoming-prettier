e5c933bd04c7465af46d2ef9f59fc4287c6bb46b
Pontus Persson <pontus.alexander@gmail.com>
2017-10-09 09:50:28 +0200

    Really ugly stuff.

--- src/CharacterCount.jsx
+++ src/CharacterCount.jsx
@@ -1,31 +1,27 @@
 import React, {Component} from 'react';
 
-class 
+class
 CharacterCount 
 extends 
 Component {
-  constructor
-  (props) {
-    super(props);
+constructor
+(props) {
+  super(props);
 
-    this.state = {
-      characterCount: 0,
-    };
-  }
+  this.state = {
+    characterCount: 0,
+  };
+}
 
-  handleInputChange=(event)=>{
-    this.setState({
-      characterCount: event.target.value.length,
-    });
-  }
+handleInputChange=(event)=>{this.setState({characterCount: event.target.value.length});}
 
-  render() {
-    return <div className="CharacterCount">
-      <input 
-      type='text'
-      onChange={this.handleInputChange}
-      >
-      </input>
-      </div>;
-  }
+render() {
+  return <div className="CharacterCount">
+    <input 
+    type='text'
+    onChange={this.handleInputChange}
+    >
+    </input>
+    </div>;
+}
 }
